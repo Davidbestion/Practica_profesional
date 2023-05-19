f1d432c26e23bb1d81a99ab4c6ab2c9297a90a56
Davidbestion <104589703+Davidbestion@users.noreply.github.com>
2023-05-19 00:04:54 -0400

    Metodo para leer archivos en una carpeta.

--- proyecto.py
+++ proyecto.py
@@ -1,4 +1,5 @@
 import gensim as gen
+import os
 from spacy.lang.es import Spanish
 from spacy.tokenizer import Tokenizer
 
@@ -7,9 +8,25 @@ from spacy.tokenizer import Tokenizer
 #import gensim.models.poincare as poinc
 #import gensim.scripts.glove2word2vec as glove #esto transforma de GloVe a word2vec
 
-nlp = Spanish()
-tokenizer = nlp.tokenizer
-tokens = tokenizer(text) 
+class WordEmbedding(object):
+    #Method that reads text from docs and returns a list with the text of each file.
+    def ReadTexts():
+        path = "path/to/directory" # change to the directory path where your text files are stored
+        doc_list = []
+        for filename in os.listdir(path):
+            if filename.endswith(".txt"):
+                with open(os.path.join(path, filename), 'r') as f:
+                    text = f.read().replace('\n', ' ')
+                    doc_list.append(text)
+        return doc_list
+      
+    def TokenizeText(Text):#Text es el texto entero de un documento.
+        nlp = Spanish()
+        tokenizer = nlp.tokenizer
+        tokens = tokenizer(Text) 
+        return tokens
+    
+
 
 
 #Method that processes a text and returns a list of the vectors of each word using word2vec
